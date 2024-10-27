1d74d4c9f932bd6d3c836391701552212b983722
LongFixBug <longnh55555@gmail.com>
2024-10-28 01:23:33 +0700

    update menu screen

--- screens/DetailMenuScreen.js
+++ screens/DetailMenuScreen.js
@@ -32,9 +32,7 @@ const DetailMenuScreen = ({ navigation }) => {
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged.
+          scrambled it to make a type specimen book.aaaaa
         </Text>
       </ScrollView>
     </>
